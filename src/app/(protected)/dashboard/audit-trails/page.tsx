8fc0c17d58cd62518839d33b1980149243156e55
tangguhriyadi <mtangguh97@gmail.com>
2025-05-29 18:25:59 +0700

    fix: fixing user log page middleware

--- src/app/(protected)/dashboard/audit-trails/page.tsx
+++ src/app/(protected)/dashboard/audit-trails/page.tsx
@@ -1,5 +1,18 @@
+import { getServerSession } from "next-auth";
 import AuditTrailPage from "../../../../features/audit-trail/audit-trail.page";
+import authConfig from "../../../../configs/auth.config";
+import { redirect } from "next/navigation";
+import RoleEnum from "../../../../enums/role.enum";
 
-export default function Page() {
-  return <AuditTrailPage />
+export default async function Page() {
+  const session = await getServerSession(authConfig);
+
+  if (!session?.token) {
+    redirect("/api/auth/signin");
+  }
+
+  if (session.user.role !== RoleEnum.ADMIN) {
+    redirect("/");
+  }
+  return <AuditTrailPage />;
 }
